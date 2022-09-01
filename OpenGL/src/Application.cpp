6cc37b6e770072a2c976419ef65305466f222e14
yuyingxue <1605746235@qq.com>
2022-09-01 10:28:39 +0800

    Vertex attributes and layouts

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -27,14 +27,18 @@ int main(void)
 
 
     float positions[6] = {
-        -0.5f, -0.5f,
-         0.0f,  0.5f,
-         0.5f, -0.5f,
+        -0.5f, -0.5f, 
+         0.0f,  0.5f, 
+         0.5f, -0.5f 
     };
     unsigned int buffer;
     glGenBuffers(1, &buffer); // generate a buffer and return an id.
     glBindBuffer(GL_ARRAY_BUFFER, buffer); // bind buffer
-    glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float),positions,GL_STATIC_DRAW); // 
+    glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float),positions,GL_STATIC_DRAW); 
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2,0); // position
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
