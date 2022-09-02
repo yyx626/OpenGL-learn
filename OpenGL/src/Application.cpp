3442ef10ce5fc768a74043ca58925065e050a4c7
yuyingxue <1605746235@qq.com>
2022-09-02 09:43:21 +0800

    Uniforms

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -7,9 +7,9 @@
 #include <sstream>
 
 #define ASSERT(x) if(!(x)) __debugbreak();
-#define GLCall(x) { GLClearError();\
+#define GLCall(x)  GLClearError();\
     x;\
-    ASSERT(GLLogCall(#x, __FILE__, __LINE__)) }
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__)) 
 
 
 static void GLClearError() {
@@ -114,6 +114,8 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(1);
+
     if (glewInit() != GLEW_OK)
         std::cout << "Error!" << std::endl;
     // now we can use all opengl functions.
@@ -148,15 +150,28 @@ int main(void)
     
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-    glUseProgram(shader); // bind shader.
+    GLCall(glUseProgram(shader)); // bind shader.
 
+    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+    ASSERT(location != -1); // can't find uniform.
+    GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
+
+    float r = 0.0f;
+    float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));  // 6是索引数量，而不是顶点数量
+        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));  // 6是索引数量，而不是顶点数量
+
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+            increment = 0.05f;
+        r += increment;
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
